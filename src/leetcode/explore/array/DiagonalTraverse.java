2551049610503e19048dd11d83bf87b1ec598445
aaron.liu <liu0013@gmail.com>
2020-05-27 15:29:23 +0800

    update

--- src/leetcode/explore/array/DiagonalTraverse.java
+++ src/leetcode/explore/array/DiagonalTraverse.java
@@ -7,7 +7,7 @@ public class DiagonalTraverse {
      *        [ 4,  5,  6]
      *        [ 7,  8,  9]
      *        [10, 11, 12]
-     *        
+     *
      * @param matrix
      * @return
      */
